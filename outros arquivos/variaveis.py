4b045d0620771ed5bb5410cd677cc561861ad920
Luis Yamagata <luisyamagata@gmail.com>
2025-09-04 13:16:52 -0300

    adicionada pasta outros arquivos e, nela arquivos aula02 e variaveis.py

---
+++ outros arquivos/variaveis.py
@@ -0,0 +1,23 @@
+# Quero declarar uma variável para guardar meu nome
+nome = "william"
+produto = "televisão" #string (letras, palavras, frases)
+quantidade = 25 #number/int (inteiro)
+preco = 1000.00 # number/float (decimal)
+em_estoque = True #boolean (True ou False)
+print(nome)
+print(produto)
+print(quantidade)
+print(em_estoque)
+
+
+#cammelCase
+# emEstoque
+
+#PascalCase
+# EmEstoque
+
+#snake_case
+# em_estoque
+
+# kebab-case
+# em-estoque
